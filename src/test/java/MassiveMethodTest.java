c5e79f3c06ba69fd635e2ca79b3f9081783ea189
Mihail_Vorobyov <ptichkinm@mail.ru>
2021-11-08 17:56:00 +0300

    Реализован класс с методом, который вытаскивает нужный массив; реализованы тесты с одним массивом и с массивом без четвёрок.

--- src/test/java/MassiveMethodTest.java
+++ src/test/java/MassiveMethodTest.java
@@ -1,4 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
 
 public class MassiveMethodTest {
-
+	
+	MassiveMethod massiveMethod;
+	
+	@Test
+	public void shouldReturnNumbersAfterLastFour() {
+		massiveMethod = new MassiveMethod();
+		Assert.assertArrayEquals(new int[] {5, 6}, massiveMethod.getNumbersAfterLastFour(new int[] {0, 1, 2, 3, 4, 5, 6}));
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void shouldReturnRuntimeException() {
+		massiveMethod = new MassiveMethod();
+		massiveMethod.getNumbersAfterLastFour(new int[] {0, 1, 2, 3, 2, 1, 0});
+	}
 }
