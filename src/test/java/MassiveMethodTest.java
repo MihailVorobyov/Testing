44c9e264ea9eb771dd908ae7477afb52f86d665b
Mihail_Vorobyov <ptichkinm@mail.ru>
2021-11-08 18:21:24 +0300

    Выполнено задание 1

--- src/test/java/MassiveMethodTest.java
+++ src/test/java/MassiveMethodTest.java
@@ -1,21 +1,45 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class MassiveMethodTest {
 	
 	MassiveMethod massiveMethod;
 	
-	@Test
-	public void shouldReturnNumbersAfterLastFour() {
-		massiveMethod = new MassiveMethod();
-		Assert.assertArrayEquals(new int[] {5, 6}, massiveMethod.getNumbersAfterLastFour(new int[] {0, 1, 2, 3, 4, 5, 6}));
+	private static Stream<Arguments> intValuesWithoutFour() {
+		return Stream.of(
+			Arguments.arguments(new int[] {1, 2, 8, 16, 32, 64, 128, 256, 512, 1024}),
+			Arguments.arguments(new int[] {0, 1, 2, 3}),
+			Arguments.arguments(new int[] {1, -4, 5, 14, 44}),
+			Arguments.arguments(new int[] {0})
+		);
 	}
 	
-	@Test(expected = RuntimeException.class)
-	public void shouldReturnRuntimeException() {
+	private static Stream<Arguments> intValues() {
+		return Stream.of(
+			Arguments.arguments(new int[] {1, 7}, new int[] {1, 2, 4, 4, 2, 3, 4, 1, 7}),
+			Arguments.arguments(new int[] {}, new int[] {4}),
+			Arguments.arguments(new int[] {}, new int[] {1, 4, 2, 4, 3, 4}),
+			Arguments.arguments(new int[] {10, 11, 9}, new int[] {-10, 4, 4, 4, 5, 4, 10, 11, 9})
+		);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("intValuesWithoutFour")
+	public void shouldReturnRuntimeExceptionWhenFourNotFound (int[] values) {
+		Assertions.assertThrows(RuntimeException.class, () -> massiveMethod.getNumbersAfterLastFour(values));
+	}
+	
+	@ParameterizedTest
+	@MethodSource("intValues")
+	public void shouldReturnValuesAfterLastFour (int[] expected, int[] real) {
 		massiveMethod = new MassiveMethod();
-		massiveMethod.getNumbersAfterLastFour(new int[] {0, 1, 2, 3, 2, 1, 0});
+		Assertions.assertArrayEquals(expected, massiveMethod.getNumbersAfterLastFour(real));
 	}
 }
